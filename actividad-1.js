9a39644dcd7fe7452499cf4e6508a0b888421cbc
philter.15 <pcruz.freyssinet@gmail.com>
2022-05-31 20:27:17 -0500

    first commit

---
+++ actividad-1.js
@@ -0,0 +1,52 @@
+/- 1- en colsola de navegador crear programa que pida al usuario ingresar un numero, multiplique ese numero por 2 y muestre en salida el
+resultado de la operacion
+
+
+var inertNum= 1
+undefined
+constant Duplicar= 2
+VM716:1 Uncaught SyntaxError: Unexpected identifier
+constant duplicar= 2 
+VM801:1 Uncaught SyntaxError: Unexpected identifier
+const multiplicador= 2
+undefined
+var total= inertNum * multiplicador
+undefined
+console.log (total)
+VM1021:1 2
+undefined
+
+
+// en sonsola de navegador escribir programa que reciba 2 numeros e imprima en pantalla la resta de ambos
+var insertnum1= 2
+undefined
+var insertnum2= 3
+undefined
+var total= insertnum1-insertnum2
+undefined
+console.log (total)
+VM1297:1 -1
+undefined
+
+//escribir programa que calcule el area de un circulo con un radio r=5.3 imprime el area en pantalla (formula a=pi*r**2)
+var radio=5.3
+undefined
+const pi=3.141516
+undefined
+var areaCirculo=pi*(radio**2)
+undefined
+console.log (areaCirculo)
+VM1531:1 88.24518444
+undefined
+
+//escribe un programa que calcule el area de un rombo, datos D= 8cm, d= 6cm, formula a=(D*d)/2
+var D=8.5
+undefined
+var d=6
+undefined
+var areaRombo= (D*d)/2
+undefined
+console.log (areaRombo)
+VM1724:1 25.5
+undefined
+
